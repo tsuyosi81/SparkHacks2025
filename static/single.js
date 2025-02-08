875ecc95fa0d205df14b48518bd5475ca17dd078
Cris <crimelsan@gmail.com>
2025-02-08 16:29:32 -0600

    YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- static/single.js
+++ static/single.js
@@ -1,5 +1,9 @@
 var editBuyer = document.getElementById("edit-buyer");
+var editAmount = document.getElementById("edit-amount");
+var editProduct = document.getElementById("edit-product");
+var editPrice = document.getElementById("edit-price");
 var extraDiv = document.getElementById("extra");
+console.log(extraDiv)
 editBuyer.onclick = ()=>{
     extraDiv.innerHTML = "";
     var newInput = document.createElement('input');
@@ -14,3 +18,44 @@ editBuyer.onclick = ()=>{
 
 }
 
+editAmount.onclick = ()=>{
+    extraDiv.innerHTML = "";
+    var newInput = document.createElement('input');
+    newInput.id = "new-amount"
+    newInput.name = "new_amount"
+    var submit = document.createElement('button');
+    submit.innerHTML = "SUBMIT"
+    submit.type = "submit";
+    
+    extraDiv.appendChild(newInput);
+    extraDiv.appendChild(submit)
+
+}
+
+editProduct.onclick = ()=>{
+    extraDiv.innerHTML = "";
+    var newInput = document.createElement('input');
+    newInput.id = "new-product"
+    newInput.name = "new_product"
+    var submit = document.createElement('button');
+    submit.innerHTML = "SUBMIT"
+    submit.type = "submit";
+    
+    extraDiv.appendChild(newInput);
+    extraDiv.appendChild(submit)
+
+}
+
+editPrice.onclick = ()=>{
+    extraDiv.innerHTML = "";
+    var newInput = document.createElement('input');
+    newInput.id = "new-price"
+    newInput.name = "new_price"
+    var submit = document.createElement('button');
+    submit.innerHTML = "SUBMIT"
+    submit.type = "submit";
+    
+    extraDiv.appendChild(newInput);
+    extraDiv.appendChild(submit)
+
+}
