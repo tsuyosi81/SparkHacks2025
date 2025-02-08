df554f83809e8b2cb061986c09870b347d379574
Tsuyoshi <tharayama81@gmail.com>
2025-02-08 16:49:32 -0600

    merged

---
+++ templates/table.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Add any JavaScript functionality for the table here
+    // For example, sorting, filtering, or pagination
+    console.log("Table page loaded!");
+});
