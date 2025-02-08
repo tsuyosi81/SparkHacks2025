f10c1a5564b273fbce8bef0a1828082573f8ca2b
Cris <crimelsan@gmail.com>
2025-02-08 10:12:04 -0600

    Added server.py

--- server.py
+++ server.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def root_path():
+    return "<h1>root</h1>"
+
+@app.route('/home')
+def home():
+    return "<h1>home</h1>"
+
+@app.route('/login')
+def login():
+    return "<h1>login</h1>"
+
+@app.route('/buyer')
+def buyer():
+    return "<h1>buyer</h1>"
+
+@app.route('/seller')
+def seller():
+    return "<h1>seller</h1>"
+
+@app.route('/seller/table')
+def seller_table():
+    return "<h1>seller table</h1>"
     
+@app.route('/seller/table/<number>')
+def seller_table_number(number):
+    return render_template("hello.html",order=number)
